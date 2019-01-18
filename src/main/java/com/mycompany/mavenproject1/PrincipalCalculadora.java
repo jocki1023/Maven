b8e12316dad3d53a3dd2152c94d1a7f90a35012d
jairo <jocki1023@gmail.com>
2019-01-18 10:24:35 -0500

    Modificacion POM.xml

---
+++ src/main/java/com/mycompany/mavenproject1/PrincipalCalculadora.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject1;
+public class PrincipalCalculadora {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora cal = new Calculadora();
+        cal.menu();
+    }
+}
